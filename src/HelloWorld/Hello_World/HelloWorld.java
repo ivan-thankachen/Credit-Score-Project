04151a12dfe43bedc193fb057d758f8a9cb1df45
IvanThankachen <ivan.thankachen04@gmail.com>
2026-04-10 13:20:18 -0500

    Setup repository
    I created src and lib folders. Inside src, there is a HelloWorld folder that contains a program that says Hello World.

---
+++ src/HelloWorld/Hello_World/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * Ivan Thankachen - Hello World Program
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
