5c64cfcc3e4d510b931eb32c2a36ef5436ba534c
IvanThankachen <ivan.thankachen04@gmail.com>
2026-04-24 17:53:00 -0500

    Commit 4: Worked on Login Class.
    I worked on the login class by initializing username and password variables. I also created the first part of greetings method by asking for user input on username and password. I need to find some way to create a level of security. My plan was to use hashMaps to encode.

--- src/Credit_Score_Project/Login.java
+++ src/Credit_Score_Project/Login.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Login here.
  *
@@ -8,26 +8,21 @@
 public class Login
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Login
-     */
-    public Login()
+    String user = "guest";
+    String password = "123";
+    Scanner receive = new Scanner(System.in);
+    
+    public Login(String u, String p)
     {
-        // initialise instance variables
-        x = 0;
+        this.user = u;
+        this.password = p;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public void greetings()
     {
-        // put your code here
-        return x + y;
+        System.out.println("Welocome to the Credit Score Program!");
+        System.out.println("Please input your username and password.");
+        this.user = receive.nextLine();
+        this.password = receive.nextLine();
     }
 }
