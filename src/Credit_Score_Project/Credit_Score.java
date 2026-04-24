9f26cf7d545509e6f3927367cf7bde1cf23d08fc
IvanThankachen <ivan.thankachen04@gmail.com>
2026-04-24 17:42:13 -0500

    Commit 3: Starting Credit Score Class
    I started working on the Credit Score class by creating the constructor with the credit provider value obtained from user. I then initialized the variables used to determine FICO scores. I wanted to create a randomize_percentages method to give all the floats a value, but I need to research a bit more on how the percentages vary between the three providers.

--- src/Credit_Score_Project/Credit_Score.java
+++ src/Credit_Score_Project/Credit_Score.java
@@ -1,12 +1,26 @@
-
+import java.util.Random; 
 /**
  * Write a description of class Credit_Score here.
  *
- * @author (your name)
+ * Ivan Thankachen
+ * Credit_Score Class
  * @version (a version number or a date)
  */
+
 public class Credit_Score
 {
     // instance variables - replace the example below with your own
+    public Credit_Score(String credit)
+    {
+        String credit_provider = credit;
+    }
+    int credit_score;
+    float payment_history;
+    float amounts_owed;
+    float credit_history;
+    float credit_mix;
+    float new_credit;
+    
+    
     
 }
