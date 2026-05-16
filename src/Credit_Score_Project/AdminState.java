bfa76010f83ee91b2e0ae137a5ba8f945d79987e
IvanThankachen <ivan.thankachen04@gmail.com>
2026-05-15 22:35:10 -0500

    Sprint 4 Push 2
    Started State Pattern for Login System.

---
+++ src/Credit_Score_Project/AdminState.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class AdminState here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AdminState implements LoginState
+{
+    // instance variables - replace the example below with your own
+    public void login()
+    {
+        
+    }
+    public void logout()
+    {
+        
+    }
+}
---
+++ src/Credit_Score_Project/LogOutState.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class LogOutState here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LogOutState implements LoginState
+{
+    // instance variables - replace the example below with your own
+
+    public void login()
+    {
+        
+    }
+    public void logout()
+    {
+        
+    }
+}
